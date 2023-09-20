d8d4d19b0906f825644607156d3b772c83ab0a98
whitemousess <thangcoiofficial@gmail.com>
2023-09-20 08:43:46 +0700

    cors

--- src/Controllers/itemController.js
+++ src/Controllers/itemController.js
@@ -3,8 +3,7 @@ const MainModel = require("../models/itemsModel");
 module.exports = {
 
   show(req, res, next) {
-    MainModel.find({})
-    .then(item => res.json(item))
+    res.json({message: "Hello"});
   }
 
 };
